7464291fa800f360e6aa022ff9f2ea83d68cafff
George J Padayatti <george.padayatti@igrant.io>
2023-09-06 14:25:30 +0530

    Upd: Add rendering tests for login component
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

---
+++ src/pages/login/Login.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { AdminContext } from 'react-admin';
+import { render, screen } from '@testing-library/react';
+
+import Login from './Login';
+
+test('renders the Govstack logo', async () => {
+    render(
+        <AdminContext>
+            <Login />
+        </AdminContext>
+    );
+    const logo = screen.getByAltText('Logo');
+    expect(logo).toBeInTheDocument();
+})
+
+test('renders the "Login to Admin Dashboard" text', async () => {
+    render(
+        <AdminContext>
+            <Login />
+        </AdminContext>
+    );
+    const text = screen.getByText('Login to Admin Dashboard');
+    expect(text).toBeInTheDocument();
+})
+
+test('renders the User ID and Password input fields', async () => {
+    render(
+        <AdminContext>
+            <Login />
+        </AdminContext>
+    );
+    const userIDInput = screen.getByPlaceholderText('User ID');
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    expect(userIDInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+})
+
+
+test('renders the "Remember Me" checkbox', async () => {
+    render(
+        <AdminContext>
+            <Login />
+        </AdminContext>
+    );
+    const checkbox = screen.getByLabelText('Remember Me');
+
+    expect(checkbox).toBeInTheDocument();
+})
