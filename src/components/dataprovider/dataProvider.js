1170244f70a3022590de611b851a7531158eb462
MARZOOQUE <mmarzooque@gmail.com>
2023-09-21 16:10:44 +0530

    Feat: #6 DA Records

--- src/components/dataprovider/dataProvider.js
+++ src/components/dataprovider/dataProvider.js
@@ -8,7 +8,6 @@ export const dataProvider = fakeDataProvider({
         { id: 3, usagePurpose: 'User Registration', version: '1.0.0', dataExchange:"Data Using Service", status:"Saved", lawfulBasisOfProcessing: "Contractual"},
         { id: 4, usagePurpose: 'User Registration', version: '1.1.0', dataExchange:"Data Source", status:"Saved", lawfulBasisOfProcessing: "Contractual"},
         { id: 5, usagePurpose: 'User Registration', version: '1.0.2', dataExchange:"Data Using Service", status:"Saved", lawfulBasisOfProcessing: "Consent"},
-
     ],
     personaldata: [
         { id: 0, dataAttributeName: 'Aadhar name', description: 'Aadhar names', dataAgreement: 'Verify Aadhaar' },
@@ -16,5 +15,12 @@ export const dataProvider = fakeDataProvider({
         { id: 2, dataAttributeName: 'Aadhar name', description: 'Aadhar names', dataAgreement: 'Verify Aadhaar' },
         { id: 3, dataAttributeName: 'Aadhar name', description: 'Aadhar names', dataAgreement: 'Verify Aadhaar' },
         { id: 4, dataAttributeName: 'Aadhar name', description: 'Aadhar names', dataAgreement: 'Verify Aadhaar' },
+    ],
+    userrecords: [
+        { id: 0, subscriberID: 'e8b045f4-5401-4722-a447-3a4d6031a492', dataAgreement: 'Market and Campaign', lawfulBasis: 'Consent', agreementEvent: 'Opt-in', timestamp:'2023-08-26T18:52:33z'},
+        { id: 1, subscriberID: 'e9b045f4-5401-4722-a447-3a4d6031a492', dataAgreement: 'Campaign', lawfulBasis: 'Consent', agreementEvent: 'Opt-in', timestamp:'2023-08-26T18:52:33z'},
+        { id: 2, subscriberID: 'e9b045f4-5401-4722-a447-3a4d6031a492', dataAgreement: 'Marketing', lawfulBasis: 'Consent', agreementEvent: 'Opt-in', timestamp:'2023-08-26T18:52:33z'},
+        { id: 3, subscriberID: 'e9b045f4-5401-4722-a447-3a4d6031a492', dataAgreement: 'Market and Campaign', lawfulBasis: 'Consent', agreementEvent: 'Opt-in', timestamp:'2023-08-26T18:52:33z'},
+        { id: 4, subscriberID: 'e9b045f4-5401-4722-a447-3a4d6031a492', dataAgreement: 'Market and Campaign', lawfulBasis: 'Consent', agreementEvent: 'Opt-in', timestamp:'2023-08-26T18:52:33z'},
     ],
 })
---
+++ src/pages/userRecords/userRecords.tsx
@@ -0,0 +1,178 @@
+import { useState } from "react";
+import { List, Datagrid, TextField, Form, TextInput } from "react-admin";
+
+import {
+  Box,
+  Typography,
+  Tooltip,
+  Radio,
+  FormControlLabel,
+  FormControl,
+  RadioGroup,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+// icons
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+
+import BreadCrumb from "../../components/Breadcrumbs";
+import DataAgreementModal from "../../components/modals/dataAgreementModal";
+import Dropdown from "../../components/dropdowns/dropdown";
+
+const Container = styled("p")(({ theme }) => ({
+  margin: "52px 15px 0 15px",
+  background: "#FFFF",
+  [theme.breakpoints.down("sm")]: {
+    margin: "52px 0 10px 0",
+  },
+}));
+
+const HeaderContainer = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexWrap: "wrap",
+  marginTop: 10,
+  width: "100%",
+});
+
+const Item = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  backgroundColor: "#fff",
+  padding: "10px 30px 10px 30px",
+  marginTop: "13px",
+  justifyContent: "space-between",
+  flexWrap: "wrap",
+  color: "#0000",
+  height: "auto",
+  borderRadius: 3,
+  border: "1px solid #E1E1E1",
+});
+
+const UserRecords = () => {
+  const [openDataAgreementModal, setOpenDataAgreementModal] = useState(false);
+
+  return (
+    <Container>
+      <List actions={false} sx={{ width: "100%", overflow: "hidden" }}>
+        <Form>
+          <BreadCrumb Link="Manage Users  /  User Records" />
+          <HeaderContainer>
+            <Typography variant="h6" fontWeight="bold">
+              User Records
+            </Typography>
+          </HeaderContainer>
+          <Box
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+            }}
+          >
+            <Typography variant="body1">
+              Do queries on the data agreement records for audit purpose
+            </Typography>
+            <TextInput
+              source="overview"
+              autoFocus
+              variant="outlined"
+              label={false}
+              placeholder="Search by Consent Agreement ID, Consent Record ID or Subscriber ID"
+              helperText={false}
+              style={{ width: "40%" }}
+            />
+          </Box>
+          <Item>
+            <Typography color="grey" variant="subtitle1">
+              Filter query
+            </Typography>
+            <FormControl
+              sx={{
+                width: "70%",
+              }}
+            >
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                defaultValue="viewall"
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <FormControlLabel
+                  value="viewall"
+                  control={<Radio color="default" />}
+                  label="View All"
+                  sx={{ color: "black" }}
+                />
+                <Box sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems:"center"
+                }}>
+                  <FormControlLabel
+                    value="filter1"
+                    control={<Radio color="default" />}
+                    label=""
+                  />
+                  <Dropdown displayValue={"Filter by Puropse"} />
+                </Box>
+                <Box sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems:"center"
+                }}>                  <FormControlLabel
+                    value="filter2"
+                    control={<Radio color="default" />}
+                    label=""
+                  />
+                  <Dropdown displayValue={"Filter by Lawful Basis"} />
+                </Box>
+              </RadioGroup>
+            </FormControl>
+          </Item>
+        </Form>
+        <Box mt={1.5}>
+          <Datagrid bulkActionButtons={false} sx={{ overflow: "auto" }}>
+            <TextField source="subscriberID" label={"Subscriber ID"} />
+            <TextField source="dataAgreement" label={"Data Agreement "} />
+            <TextField source="lawfulBasis" label={"Lawful Basis"} />
+            <TextField source="agreementEvent" label={"Agreement Event"} />
+            <TextField source="timestamp" label={"Timestamp"} />
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              <Tooltip title="View Data Agreement" placement="top">
+                <RemoveRedEyeOutlinedIcon
+                  onClick={() => setOpenDataAgreementModal(true)}
+                  fontSize="small"
+                  color="disabled"
+                  style={{ cursor: "pointer" }}
+                />
+              </Tooltip>
+            </Box>
+          </Datagrid>
+        </Box>
+      </List>
+      {/* Modals */}
+
+      {/* Read Data agreement */}
+      <DataAgreementModal
+        open={openDataAgreementModal}
+        setOpen={setOpenDataAgreementModal}
+        mode={"Read"}
+      />
+    </Container>
+  );
+};
+
+export default UserRecords;
