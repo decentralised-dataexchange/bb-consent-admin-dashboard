2151065df824b6595d6639eace17723a0889017c
MARZOOQUE <mmarzooque@gmail.com>
2023-10-25 00:10:12 +0530

    Feat: #88 Integrate Appbar menu API

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,15 +1,8 @@
 export interface User {
-  ID: string;
-  Name: string;
-  IamID: string;
-  Email: string;
-  Phone: string;
-  ImageID: string;
-  ImageURL: string;
-  LastVisit: string;
-  Client: any;
-  Orgs: any[];
-  APIKey: string;
-  Roles: any[];
-  IncompleteProfile: boolean;
+  id: string
+  email: string
+  name: string
+  avatarImageId: string
+  avatarImageUrl: string
+  lastVisited: string
 }
--- src/service/localStorageService.tsx
+++ src/service/localStorageService.tsx
@@ -5,12 +5,12 @@ export const LocalStorageService = {
   updateToken: (token: AccessToken) => {
     localStorage.setItem("Token", JSON.stringify(token));
   },
-  // updateUser: (user: User) => {
-  //   localStorage.setItem("User", JSON.stringify(user));
-  // },
-  // getUser: (): User => {
-  //   return JSON.parse(localStorage.getItem("User")!);
-  // },
+  updateUser: (user: User) => {
+    localStorage.setItem("User", JSON.stringify(user));
+  },
+  getUser: (): User => {
+    return JSON.parse(localStorage.getItem("User")!);
+  },
   getAccessToken: () => {
     return JSON.parse(localStorage.getItem("Token")!)?.accessToken;
   },
@@ -19,6 +19,13 @@ export const LocalStorageService = {
   },
   clear: () => {
     localStorage.removeItem("Token");
-    // localStorage.removeItem("User");
+    localStorage.removeItem("User");
+    localStorage.removeItem("Avatar");
+  },
+  updateProfilePic: (avatar: any) => {
+    localStorage.setItem("Avatar", JSON.stringify(avatar));
+  },
+  getUserProfilePic: (): any => {
+    return JSON.parse(localStorage.getItem("Avatar")!);
   },
 };
